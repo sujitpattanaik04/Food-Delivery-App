b3c1900667050ce400b90c4f42a440d4d3b94a0e
Sujit Pattanaik <sujit.pattanaik@argusoft.in>
2024-10-06 23:59:12 +0530

    .env file added to .gitignore file

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = require("./app");
 const port = process.env.PORT || 3000;
 
 // Create Server
-const server = app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.SERVER_PORT, () => {
   console.log("server has started...");
   console.log(`Server is listening on port ${process.env.PORT}`);
 });
